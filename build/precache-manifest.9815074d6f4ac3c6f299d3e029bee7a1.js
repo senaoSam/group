e14e84f0753d924e6ed719ae2f3e2ec0ad05a641
莊逸軒 <sam.chuang@senao.com>
2021-12-13 17:50:27 +0800

    build

---
+++ build/precache-manifest.9815074d6f4ac3c6f299d3e029bee7a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b418eb729728a74b9f5b4dd14926f06",
+    "url": "/aram/build/index.html"
+  },
+  {
+    "revision": "a8ea97a171dbc45fb4c9",
+    "url": "/aram/build/static/css/main.532521e0.chunk.css"
+  },
+  {
+    "revision": "58c4630413ee0496f1c6",
+    "url": "/aram/build/static/js/2.b802e031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aram/build/static/js/2.b802e031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ea97a171dbc45fb4c9",
+    "url": "/aram/build/static/js/main.75afc5d8.chunk.js"
+  },
+  {
+    "revision": "730df67392ce176e016c",
+    "url": "/aram/build/static/js/runtime-main.2941ade4.js"
+  },
+  {
+    "revision": "69e6808a718500e6ec574e447cdcd488",
+    "url": "/aram/build/static/media/pulo.69e6808a.png"
+  }
+]);
